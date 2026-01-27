278009351f70a8177537d89059f949c9aad82ae5
Flor Hoyos <79953103+FlorHoyos@users.noreply.github.com>
2026-01-27 13:59:53 -0600

    Add files via upload

---
+++ patient_coverage_and_journey.sql
@@ -0,0 +1,23 @@
+-- Which patients have never had an appointment scheduled
+
+SELECT p.FName AS First_Name, p.LName AS Last_Name, p.patient_id 
+FROM Patients p
+LEFT JOIN Appointment a ON p.patient_id = a.patient_id 
+WHERE a.appointment_id IS NULL
+ORDER BY patient_id ASC ;
+
+-- For each patient, what was their first ever appointment
+-- date and doctor
+
+SELECT a.patient_id, a.appointment_date::date AS first_appt, d.doct_id, d.fname, d.lname
+FROM Appointment a 
+JOIN doctor d ON d.doct_id = a.doct_id
+ORDER BY a.patient_id, first_appt ASC, a.appointment_id ASC;
+
+
+
+
+
+
+
+
